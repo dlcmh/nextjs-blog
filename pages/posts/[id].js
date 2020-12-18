dcb5cb9b4bf92e943e8633859696653f02c4cfbc
David Chin <dlcmhd@me.com>
2020-12-18 10:03:36 +0800

    date-fns, use styles

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head';
+import { Date } from '../../components/date';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css'
 
 // Called first
 export async function getStaticPaths() {
@@ -25,13 +28,18 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>
+          {postData.title}
+        </h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }

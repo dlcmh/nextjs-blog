cc9e3e0872f5ba366b06b0785cdc9ea048bb14cc
David Chin <dlcmhd@me.com>
2020-12-18 08:45:00 +0800

    dynamic routes -> [id].js -> getStaticPaths -> getStaticProps

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+// Called first
+export async function getStaticPaths() {
+  console.log('getStaticPaths called');
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// Called second
+export async function getStaticProps({ params }) {
+  console.log('getStaticProps', { params });
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
